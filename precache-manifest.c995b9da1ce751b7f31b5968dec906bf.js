e8f2ea8f384f1e077a74aa9902846ddacd0e5808
Saicharan67 <mahankalisaicharan@gmail.com>
2020-12-31 12:37:30 +0530

    deploy

---
+++ precache-manifest.c995b9da1ce751b7f31b5968dec906bf.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcba08a9a665f7c4242",
+    "url": "/DevelopersPoint.org/css/app.cfc11b15.css"
+  },
+  {
+    "revision": "1cdc0063e6d10aa01b6f",
+    "url": "/DevelopersPoint.org/css/chunk-329f5bc8.5cda8203.css"
+  },
+  {
+    "revision": "cd93ee8b05b8259b990c",
+    "url": "/DevelopersPoint.org/css/chunk-47a3c727.a03e82df.css"
+  },
+  {
+    "revision": "32ea9c4e940e0f86bf3e",
+    "url": "/DevelopersPoint.org/css/chunk-51bf14a6.c4fc549f.css"
+  },
+  {
+    "revision": "6de6bdfcb890cce3cfdc",
+    "url": "/DevelopersPoint.org/css/chunk-73c93d36.300cce80.css"
+  },
+  {
+    "revision": "c4d0899487d0ff100c8c42edda22ab8e",
+    "url": "/DevelopersPoint.org/favicon 2.png"
+  },
+  {
+    "revision": "9476a06d4c2a640b26f3742319e43ce7",
+    "url": "/DevelopersPoint.org/favicon 3.png"
+  },
+  {
+    "revision": "dcb0ab7d5a6f35072c177b181a8772ec",
+    "url": "/DevelopersPoint.org/favicon.png"
+  },
+  {
+    "revision": "34cf3b53689abf9b72cfbb57c4f04f96",
+    "url": "/DevelopersPoint.org/fonts/googlesans-medium.34cf3b53.woff"
+  },
+  {
+    "revision": "e0531fa74246e39c88c289778705726a",
+    "url": "/DevelopersPoint.org/fonts/googlesans-regular.e0531fa7.woff"
+  },
+  {
+    "revision": "dbe23c80f3546a76cd2b1587cf2b1889",
+    "url": "/DevelopersPoint.org/icons/icon-128x128.png"
+  },
+  {
+    "revision": "8cf4bbf1d4df5d01ac5f1e48d3c86c6b",
+    "url": "/DevelopersPoint.org/icons/icon-144x144.png"
+  },
+  {
+    "revision": "726fb9b86d499a6be5b7c52d734a0db6",
+    "url": "/DevelopersPoint.org/icons/icon-152x152.png"
+  },
+  {
+    "revision": "8547d5028f2670012ba67b4c1f213cc3",
+    "url": "/DevelopersPoint.org/icons/icon-192x192.png"
+  },
+  {
+    "revision": "d6ba3009709f7780703f26a4e0aee115",
+    "url": "/DevelopersPoint.org/icons/icon-384x384.png"
+  },
+  {
+    "revision": "8bb05c4f3cd74bc6d81d5d927852e00e",
+    "url": "/DevelopersPoint.org/icons/icon-512x512.png"
+  },
+  {
+    "revision": "50afb883b3393c388ea6428d71818131",
+    "url": "/DevelopersPoint.org/icons/icon-72x72.png"
+  },
+  {
+    "revision": "989f42bd0acb9cd5fe9bb82a007003fe",
+    "url": "/DevelopersPoint.org/icons/icon-96x96.png"
+  },
+  {
+    "revision": "61508769be082d64b722d4ec802fcad8",
+    "url": "/DevelopersPoint.org/img/Dp.61508769.png"
+  },
+  {
+    "revision": "b87bea3fe51b147ff2e2b3287ecdb09d",
+    "url": "/DevelopersPoint.org/img/Dp.b87bea3f.svg"
+  },
+  {
+    "revision": "3912410d8a670bb43941039faa9ed02d",
+    "url": "/DevelopersPoint.org/img/actions-on-google.3912410d.svg"
+  },
+  {
+    "revision": "4d26a5a38dd87e61c9b4b6e5a982f3f2",
+    "url": "/DevelopersPoint.org/img/android.4d26a5a3.svg"
+  },
+  {
+    "revision": "06fe808da0e7b0c3a5b2db9959b1fb65",
+    "url": "/DevelopersPoint.org/img/angular.06fe808d.svg"
+  },
+  {
+    "revision": "dc8af8eb3cb3417f0d0a51346750e80c",
+    "url": "/DevelopersPoint.org/img/bootcamp.dc8af8eb.jpeg"
+  },
+  {
+    "revision": "83da3df747f7bdc74fff2e4055bb3c16",
+    "url": "/DevelopersPoint.org/img/chapter.83da3df7.png"
+  },
+  {
+    "revision": "e6e9e1e9023bb27c00dbf78ad68ecc1c",
+    "url": "/DevelopersPoint.org/img/codelabs.e6e9e1e9.svg"
+  },
+  {
+    "revision": "9cb4f0f4e313b4c8249f866b86cbbc37",
+    "url": "/DevelopersPoint.org/img/dialogflow.9cb4f0f4.svg"
+  },
+  {
+    "revision": "07f6b59a1ea936cd3f2277705ddb1055",
+    "url": "/DevelopersPoint.org/img/empower.07f6b59a.svg"
+  },
+  {
+    "revision": "0d15d2051f2b2f645d2e2cf273415785",
+    "url": "/DevelopersPoint.org/img/enlighten.0d15d205.svg"
+  },
+  {
+    "revision": "130c436aa8592dca02af61cee918f10a",
+    "url": "/DevelopersPoint.org/img/err.130c436a.svg"
+  },
+  {
+    "revision": "48d7bf8a374cc32b71a72bcf6e9ffb84",
+    "url": "/DevelopersPoint.org/img/exploreml.48d7bf8a.jpeg"
+  },
+  {
+    "revision": "f92dc14cec1b6e30b87d0b0e7febcc20",
+    "url": "/DevelopersPoint.org/img/feature-image.f92dc14c.svg"
+  },
+  {
+    "revision": "6461a8b19ea70a8a0a313f4b88cd1fce",
+    "url": "/DevelopersPoint.org/img/feature.6461a8b1.png"
+  },
+  {
+    "revision": "6ad8abf960fe3d8cb76229e368e83f83",
+    "url": "/DevelopersPoint.org/img/featured-web.6ad8abf9.png"
+  },
+  {
+    "revision": "66ed57351334c0ebb3e2b7fc30a54b12",
+    "url": "/DevelopersPoint.org/img/firebase.66ed5735.svg"
+  },
+  {
+    "revision": "daeaf6be9b459c8b399ef6dc913d47ec",
+    "url": "/DevelopersPoint.org/img/flutter.daeaf6be.svg"
+  },
+  {
+    "revision": "393f75d123082c4f1f5493a19a5c41de",
+    "url": "/DevelopersPoint.org/img/google-apps.393f75d1.svg"
+  },
+  {
+    "revision": "4811104497cacee4424905d5f873d752",
+    "url": "/DevelopersPoint.org/img/google-cloud-platform.48111044.svg"
+  },
+  {
+    "revision": "8fcbba5795e8eb8ceaa17d1bf860f43b",
+    "url": "/DevelopersPoint.org/img/hackathon.8fcbba57.jpeg"
+  },
+  {
+    "revision": "2f324173fc1039f4b1fdc006c706b5e3",
+    "url": "/DevelopersPoint.org/img/impact.2f324173.png"
+  },
+  {
+    "revision": "73ef8ed662c592cf5c06cd976480901b",
+    "url": "/DevelopersPoint.org/img/impact1.73ef8ed6.jpg"
+  },
+  {
+    "revision": "a6dbd1afdace8eda883f7233711c55de",
+    "url": "/DevelopersPoint.org/img/logo-white.a6dbd1af.svg"
+  },
+  {
+    "revision": "bd00c03f6adf70faf51e7374cc0aa963",
+    "url": "/DevelopersPoint.org/img/logo.bd00c03f.svg"
+  },
+  {
+    "revision": "55666d00e4227dad0c216b4962c6ecde",
+    "url": "/DevelopersPoint.org/img/next.55666d00.png"
+  },
+  {
+    "revision": "429f457776ed937e6513d89169fbf73d",
+    "url": "/DevelopersPoint.org/img/project-pic.429f4577.png"
+  },
+  {
+    "revision": "5bb20d5660599e6cb1759e96abae5411",
+    "url": "/DevelopersPoint.org/img/showcase.5bb20d56.jpeg"
+  },
+  {
+    "revision": "3ec57642f11f5bad936c82bd421a93cc",
+    "url": "/DevelopersPoint.org/img/social-care.3ec57642.svg"
+  },
+  {
+    "revision": "134b7873c3e745498f02af6f709e2f31",
+    "url": "/DevelopersPoint.org/img/soon.134b7873.svg"
+  },
+  {
+    "revision": "fb52fe646d1f5393039edb70fa9622fe",
+    "url": "/DevelopersPoint.org/img/study-jams.fb52fe64.jpeg"
+  },
+  {
+    "revision": "d037730f5cdb4c67d41940fc94bb3135",
+    "url": "/DevelopersPoint.org/img/talk.d037730f.jpeg"
+  },
+  {
+    "revision": "a2e3932489473032643fbc77ac43d307",
+    "url": "/DevelopersPoint.org/img/talks.a2e39324.svg"
+  },
+  {
+    "revision": "9fcaa49e3d3654015c6096e1f465b664",
+    "url": "/DevelopersPoint.org/img/tech-solutions.9fcaa49e.svg"
+  },
+  {
+    "revision": "21bcd7a966baa36d1f9909300362c5ff",
+    "url": "/DevelopersPoint.org/img/tensorflow.21bcd7a9.svg"
+  },
+  {
+    "revision": "9e4510caaccc4f464da3ff80709927f9",
+    "url": "/DevelopersPoint.org/img/webinars.9e4510ca.svg"
+  },
+  {
+    "revision": "6e3722a7fae4ab3ed9496478d288b246",
+    "url": "/DevelopersPoint.org/img/wip.6e3722a7.svg"
+  },
+  {
+    "revision": "03dbde5cfeea5692cfb3f5481dcbf9bb",
+    "url": "/DevelopersPoint.org/img/workshop.03dbde5c.jpeg"
+  },
+  {
+    "revision": "311051bdfe7a60fa5649aa8df9d399d1",
+    "url": "/DevelopersPoint.org/index.html"
+  },
+  {
+    "revision": "4bcba08a9a665f7c4242",
+    "url": "/DevelopersPoint.org/js/app.4fa0662b.js"
+  },
+  {
+    "revision": "1cdc0063e6d10aa01b6f",
+    "url": "/DevelopersPoint.org/js/chunk-329f5bc8.f6186b83.js"
+  },
+  {
+    "revision": "cd93ee8b05b8259b990c",
+    "url": "/DevelopersPoint.org/js/chunk-47a3c727.7b4f30d4.js"
+  },
+  {
+    "revision": "32ea9c4e940e0f86bf3e",
+    "url": "/DevelopersPoint.org/js/chunk-51bf14a6.017c8c44.js"
+  },
+  {
+    "revision": "6de6bdfcb890cce3cfdc",
+    "url": "/DevelopersPoint.org/js/chunk-73c93d36.cc3b09da.js"
+  },
+  {
+    "revision": "a8b9d8743974c3404862",
+    "url": "/DevelopersPoint.org/js/chunk-vendors.56419a9f.js"
+  },
+  {
+    "revision": "fd30761de39070e5620d67601d9c2e75",
+    "url": "/DevelopersPoint.org/manifest.json"
+  },
+  {
+    "revision": "2b3249861e1c6ea87e463aa34e80a0b6",
+    "url": "/DevelopersPoint.org/metadata.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DevelopersPoint.org/robots.txt"
+  }
+]);
